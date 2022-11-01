7fd8b5b769db143fd4ddddc65d81fd12d3f72930
andrew <andrewasmit@hotmail.com>
2022-10-31 21:17:31 -0500

    first commit

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,10 @@
 function reverseString(str) {
-  // type your code here
+    let reverseArr = [];
+    for (char of str){
+        reverseArr.unshift(char);
+    } return reverseArr.join('');
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +21,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Write a function that takes in 1 arguyment (a string) and outputs the string in reverse
+// Using iteration, make every character of the string an array element, 
+// then .push() it into a new array (so it is added to the end and then will consequently be in reverse after the full iteration)
+// Then, .unshift() it 
